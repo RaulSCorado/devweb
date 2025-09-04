18898d82c9509893869d43793a5bb0ad1ff18fa3
Raul <raulcatupiry@gmail.com>
2025-09-04 09:39:13 -0300

    outro commit

---
+++ atividades/contador/script.js
@@ -0,0 +1,16 @@
+let numero = 0;
+
+function aumentar() {
+    numero++;
+    document.getElementById("contador").textContent = numero;
+}
+
+function diminuir() {
+    numero--;
+    document.getElementById("contador").textContent = numero;
+}
+
+function x2() {
+    let = numero *= 2
+    document.getElementById("contador").textContent = numero;
+}
